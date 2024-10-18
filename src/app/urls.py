11a17172467dc10501ab3ee86bdf9af3783b8e7d
laaraujo <lautaroarielaraujo@gmail.com>
2024-10-18 14:42:57 -0300

    add jwt auth and user-related endpoints with djoser

--- src/app/urls.py
+++ src/app/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("auth/", include("djoser.urls")),
+    path("auth/", include("djoser.urls.jwt")),
     path(
         "docs/schema.yml",
         SpectacularAPIView.as_view(
